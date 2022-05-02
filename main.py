e9e4e73a4ddf16fc6d14ed34c9e4f7c905532f5f
Lee Azzarello <lee.azzarello@astraspace.com>
2022-05-01 20:27:41 -0700

    add timetracking and a unsorted search that works!

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import sys
+from fast_autocomplete import AutoComplete,autocomplete_factory
 
 i_terminator = '#'
 i_file_contents = ""
-i_num_phrases = 0
-i_num_characters = 0
 phrase_freq = {}
 space_replacement = '_'
+words = {}
+split_character = ','
+# we don't know the values of these until we call the functions
+dict_offset = 0
+input_offset = 0
 
 for line in sys.stdin:
     i_file_contents += line
@@ -13,38 +17,60 @@ for line in sys.stdin:
 # splitlines into a list, but this can totally be done up there ^^
 i_splitlines = i_file_contents.splitlines()
 
-def get_num_phrases(input):
-    # return the first line of the input and stop
-    for line in input:
-        i_num_phrases = input
-        # funny hack
-        break
-    return i_num_phrases
+def get_words(i):
+    last = int(i[0]) + 1
+    dict_offset = last
+    # a little weird we set this here but...why not. prolly should rename the function
+    input_offset = dict_offset + 1
+    return i[1:last]
 
-def get_frequencies(input):
-    # input is a string seperated by a comma, one line at a time
-    # return a dict with form { 'phrase' : frequency }
-    split_input = input.split(',')
-    phrase_freq[split_input[0]] = split_input[1] 
-    return phrase_freq
+def format_words(i):
+    i_w = get_words(i)
+    for i in i_w:
+        w = i.split(split_character)
+        words[w[0]] = {}
+    return words
+    
+def call_autocomplete(words):
+    return AutoComplete(words=words)
 
-def simulated_input_characters(input):
-    # count i_num_phrases + 1 offset from input
-    # i_num_characers is the next line
-    # read i_num_characters into a string
-    # split the string on i_terminator character
-    inputs = input.split(i_terminator)
-
-# not really necessary but nice
-def replace_underscore_to_space(phrase):
-    return phrase.replace(space_replacement, ' ')
-
-def output(input):
-    # output should be i_num_characters lines long with a blank line for each occurance of i_terminator
-    return input
+def output(i):
+    words = format_words(i)
+    # inputs = get_input(i)
+    autocomplete = call_autocomplete(words)
+    # well that's nice. It does a search. Time to actually get input.
+    return autocomplete.search(word='v')
 
 def main():
     print(output(i_splitlines))
+
+# push the old stuff into a class so I can read better
+class NoLibraryVersion():
+    def __init__(self):
+        self.foo = 0
+
+    def get_num_phrases(self, i):
+        # return the first line of the input and stop
+        i_num_phrases = i[0]
+        return i_num_phrases
+
+    def get_frequencies(self, i):
+        # input is a string seperated by a comma, one line at a time
+        # return a dict with form { 'phrase' : frequency }
+        split_input = input.split(',')
+        phrase_freq[split_input[0]] = split_input[1] 
+        return phrase_freq
+
+    def simulated_input_characters(self, i):
+        # count i_num_phrases + 1 offset from input
+        # i_num_characers is the next line
+        # read i_num_characters into a string
+        # split the string on i_terminator character
+        inputs = i.split(i_terminator)
+
+    # not really necessary but nice
+    def replace_underscore_to_space(self, phrase):
+        return phrase.replace(space_replacement, ' ')
 
 if __name__ == "__main__":
     main()
